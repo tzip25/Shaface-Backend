f1525dbec33cb22edff2b7222590c34b09291b0b
github username <github email address>
2019-05-23 18:01:10 -0400

    config heroku settings

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_16_134653) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "actor_movies", force: :cascade do |t|
     t.integer "actor_id"
     t.integer "movie_id"
---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,6 @@
+# desc "tweets to twitter.com/blockwork_cc"
+# task update_twitter_feed: :environment do
+#   puts "Updating @blockwork_cc's twitter feed..."
+#   TwitterBot.fire_tweet
+#   puts "tweet shipped!"
+# end
